779f1c899af2491a1e22867b54f5c44135d4f0a2
ES <emsta24@gmail.com>
2015-06-03 22:00:03 +1200

    tidied up controller documents

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,19 @@
-class PageController < ApplicationController
+class BookingsController < ApplicationController
   def tables
     @booking = Booking.new
   end
 
-  def
-   @booking = Booking.create
-
-  end
-
-@booking.save
+  # def
+  #  @booking = Booking.create
   # end
 end
+
+
+#
+# # booking.create, add details from booker / user
+#
+#   def booking
+#   Booking.create(phone_number)
+#
+#   end
+# end
