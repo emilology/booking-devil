ad7587ce6110186c75e429411f882b62b26c87c3
ES <emsta24@gmail.com>
2015-06-03 23:33:12 +1200

    fixed form and controllers site almost works

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,25 +1,36 @@
 class PageController < ApplicationController
-  def tables
+  def booking
     @booking = Booking.new
   end
 
-  def new
-    @booking = Booking.new
-  end
-
-  def create
-    @booking = Booking.new(params[:booking])
-    if @booking.save
-      flash[:booking_id] = @booking.id
-      redirect_to "www.hillsidekitchen.co.nz"
+  def booking.create
+    if @booking = Booking.create(params[:booking])
+      # success
     else
-      render :action => "new"
+      # error handling
     end
   end
-  def show
-  @booking = Booking.find(params[:id])
-  redirect_to @booking.url
+
+  def initialize(attributes = {})
+    assign_attributes(attributes)
   end
 
-
+  def assign_attributes(values, options = {})
+    sanitize_for_mass_assignment(values, options[:as]).each do |k, v|
+      send("#{k}=", v)
+  end
 end
+  #
+  # def create
+  #   @booking = Booking.new(params[:booking])
+  #   if @booking.save
+  #     flash[:booking_id] = @booking.id
+  #     redirect_to "www.hillsidekitchen.co.nz"
+  #   else
+  #     render :action => "new"
+  #   end
+  # end
+  # def show
+  # @booking = Booking.find(params[:id])
+  # redirect_to @booking.url
+  # end
