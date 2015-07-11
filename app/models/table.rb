bc8be5c452ac22a43e3dcd5f7715556ddf3f6bc0
ES <emsta24@gmail.com>
2015-07-11 17:44:53 +1200

    starting to do tables stuff

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,2 +1,15 @@
 class Table < ActiveRecord::Base
+
+# if combinable?
+#
+#
+#
+
+  def combinable?
+    # returns true if it exists as a combination and its other table is free
+  end
+
+  def divisible?
+
+  end
 end
