1d7af047b8688457a0f247e60454870ce8a7f963
ES <emsta24@gmail.com>
2015-08-27 22:08:02 +1200

    added the ability to display available dates & times for that number of visitors to the booking devil

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,7 @@
 class BookingsController < ApplicationController
   @@timeslots = [2.hours, 2.hours, 150.minutes, 150.minutes, 3.hours, 210.minutes, 210.minutes, 210.minutes, 250.minutes, 250.minutes]
-  @@booking_times = (17..21).to_a
-
+  @@slot_times = (17.hours..21.hours)
+  @@booking_times = (@@slot_times.step(30.minutes).each {|t| puts t})
   before_action :load_booking, only: [:show, :update, :edit, :destroy]
   before_action :load_wizard, only: [:new, :edit, :create, :update]
 
@@ -35,25 +35,71 @@ class BookingsController < ApplicationController
 
     ## which possible start times do I have? -> @@booking_times
 
+    ##ask table of busy times which times are free for that number of visitors for all times & dates
+
+    ### output these dates
+
+    #### run times for that date
+    available_table_times=[]
     ## which table has that amount of time available starting at those times?
     ## -->
     ### for each table
-    #### get bookings
-    #### check, for each start time, if its interval (start time -- start time + needed_time) overlaps any existing booking
-    #### if it doesn't, save it for returning it
-    available_times = []
-    #### available_times.push(start_time)
+    tables.each do |table|
+      #### get bookings
+      #### check, for each start time, if its interval (start time -- start time + needed_time) overlaps any existing booking
+      busy = table.busytimes
+      #### if it doesn't, save it for returning it
+      #### available_times.push(start_time)
+      free = @@booking_times.to_a-busy
+      #####define all available timeslots with period of time free
+      needed_intervals = (needed_time/30.minutes)
+      available_table_times + (findFreeWithNeededTime(free, needed_intervals))
+      ## for each booking add end time dependent on needed_time
 
-    ## for each booking add end time dependent on needed_time
-    ## look into partial / SQL view for database to see if you can make new table saying "tables free this night for 2s / 4s / 6s if this process is too slow."
-
-    render json: {times:available_times, date: booking_date, tables: tables, needed_time: needed_time}
-
+      ## look into partial / SQL view for database to see if you can make new table saying "tables free this night for 2s / 4s / 6s if this process is too slow."
+    end
+    render json: {times:available_table_times, date: booking_date, tables: tables, needed_time: needed_time}
 
     # in your datepicker, if you choose the jquery-ui one at least, you'll have a function that will run
     # before enabling a day. In that function you'll have to call this endpoint to figure out if you have availability for
     # the day and the amount of guests and, based on this response, choose to grey out the day or not.
     # Remember to grey out sundays and mondays every time.
+  end
+
+  def findFreeWithNeededTime (free, needed_intervals)
+
+    #build consecutive time arrays
+    consecutive_intervals = consecutiveTimeslots(free)
+
+    ##reject those arrays that are shorter than needed_intervals
+    # consecutive_intervals.select! {|interval| interval.length > needed_intervals}
+    ###take amount of needed timeslots minus 1 from end of each accepted array
+    free_intervals = []
+    consecutive_intervals.each {|i| free_intervals + (i[0..-needed_intervals])}
+    ##### merge all arrays and return time
+
+    #free = @@booking_times.reject(busy)
+    #needed_time = @@timeslots[guests-1]
+    free_intervals
+  end
+
+  def consecutiveTimeslots (free)
+    #first array variable - set up initial variables
+    consecutive_intervals = []
+    previous = free.first
+    temp_array = []
+    temp_array << free.first
+
+    free[1..-1].each do |time|
+      if time-previous != 30.minutes
+        consecutive_intervals << temp_array
+        temp_array = []
+      end
+
+      temp_array << time
+      previous = time
+    end
+    consecutive_intervals
   end
 
   def edit
--- app/models/table.rb
+++ app/models/table.rb
@@ -1,11 +1,31 @@
 class Table < ActiveRecord::Base
-belongs_to :combination, class: Table
+  belongs_to :combination, class: Table
+  has_many :seating_arrangements
+  has_many :bookings, through: :seating_arrangements
 
+  def busytimes
+    #### get all busy times of bookings
+    busytimes = []
+    self.bookings.each do |booking|
+      time = booking.time
+      endtime = booking.endtime
+      #work out busy times for each day
+      start_timeslot = getTimeslot(time)
+      end_timeslot = getTimeslot(endtime)
+      #turn this into an array of busy times between start & end time (inclusive)
+      busytimes + ((start_timeslot..end_timeslot).step(30.minutes).to_a)
+    end
+    #### check, for each start time, if its interval (start time -- start time + needed_time) overlaps any existing booking
+  end
   # def combinable?
   #  returns true if it exists as a combination and its other table is free
   # end
   #
   # def divisible?
-  #
   # end
+
+  private
+  def getTimeslot (time)
+    time.hour.hours+time.min.minutes
+  end
 end
