bbb5252d6ce0efd59eae8711ff6192418d783d0f
ES <emsta24@gmail.com>
2015-09-07 11:23:05 +1200

    added third step for confirm booking and validated phone number

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,6 +6,7 @@ class Booking < ActiveRecord::Base
 
   validates :date, :time, presence: true, if: :step1?
   validates :number_of_visitors, :numericality => { :less_than_or_equal_to => 10 }, presence: true, if: :step1?
+  validates_format_of :phone_number, :with => /[0-9]/, :on => :create, if: :step2?
   validates :dietary_preferences, :name, :phone_number, presence: true, if: :step2?
 
   include MultiStepModel
